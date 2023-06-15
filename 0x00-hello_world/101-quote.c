74d6add59e34dd9d95e96581ed0f653af9d0d108
Ozioma EMEREM <code.emerem@gmail.com>
2023-06-16 00:44:23 +0100

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - The entry point of the program
+ *
+ * Return: Always returns 0 (Success)
+ */
+int main(void)
+{
+	freopen(NULL, "w", stdout);
+	puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+
+	return 1;
+}
