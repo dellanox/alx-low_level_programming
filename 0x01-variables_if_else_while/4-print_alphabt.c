90057855f97b9272b3b24c993157e2407cb4906d
Ozioma EMEREM <code.emerem@gmail.com>
2023-06-16 23:29:55 +0100

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+/**
+ * main - Entry point of this program
+ *
+ * Description: This program firstly prints the alphabet in lowercase,
+ * excluding the letters 'q' and 'e', followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter; /* Variable to store the current letter */
+
+	/* Loop through the alphabet */
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		/* Check if the letter is not 'q' and not 'e' */
+		if (letter != 'q' && letter != 'e')
+		{
+			/* Print the current letter */
+			putchar(letter);
+		}
+	}
+
+	/* Print a newline character */
+	putchar('\n');
+
+	return (0);
+}
