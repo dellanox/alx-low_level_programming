1f54a22ad1ee5b8dbf0f5030e7a87eac5461410d
Ozioma EMEREM <code.emerem@gmail.com>
2023-06-17 00:04:12 +0100

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -3,18 +3,22 @@
 /**
  * main - Entry point of the program
  *
- * Description: This program prints the lowercase alphabet in reverse,
+ * Description: This program prints all numbers of base 16 in lowercase,
  * followed by a new line.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char letter;
+	int number;
 
-	for (letter = 'z'; letter >= 'a'; letter--)
+	for (number = 0; number < 10; number++)
 	{
-	putchar(letter);
+	putchar(number + '0');
+	}
+	for (number = 'a'; number <= 'f'; number++)
+	{
+	putchar(number);
 	}
 	putchar('\n');
 
