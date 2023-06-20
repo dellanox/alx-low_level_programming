8aa27b260841644f2d473cf60e960983be736df3
Ozioma EMEREM <code.emerem@gmail.com>
2023-06-20 22:45:43 +0100

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,10 +12,10 @@ int print_last_digit(int n)
 	int last_digit;
 
 	if (n < 0)
-		n = labs(n);
+		n = -n;
 
 	last_digit = n % 10;
 	_putchar(last_digit + '0');
 
-	return (last_digit);
+	return last_digit;
 }
