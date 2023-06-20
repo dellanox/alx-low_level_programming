dd2818d4e88c2e667c05fcd6e18e3ec4facaa82d
Ozioma EMEREM <code.emerem@gmail.com>
2023-06-20 23:27:45 +0100

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,21 +2,18 @@
 #include <limits.h>
 #include "main.h"
 
-/**
- * main - check the code .
- *
- * Return: Always 0.
- */
-int main(void)
+#include "main.h"
+
+int print_last_digit(int n)
 {
-	int r;
-	r = print_last_digit(INT_MAX);
-	_putchar('0' + r);
-	_putchar('\n');
+    int last_digit;
 
-	r = print_last_digit(INT_MIN);
-	_putchar('0' + r);
-	_putchar('\n');
+    last_digit = n % 10;
 
-	return (0);
+    if (last_digit < 0)
+        last_digit = -last_digit;
+
+    _putchar(last_digit + '0');
+
+    return last_digit;
 }
