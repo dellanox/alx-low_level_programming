5871d8f86df4b6b8e2b468980e96cedf73f44e90
Ozioma EMEREM <code.emerem@gmail.com>
2023-06-21 00:46:23 +0100

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,34 +6,33 @@
  */
 void times_table(void)
 {
-    int i, j, result;
+	int i, j, result;
 
-    for (i = 0; i <= 9; i++)
-    {
-        for (j = 0; j <= 9; j++)
-        {
-            result = i * j;
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			result = i * j;
 
-            if (j == 0)
-            {
-                _putchar('0' + result);
-            }
-            else
-            {
-		if (result < 10)
-                    _putchar(' ');
-                else
-                    _putchar('0' + result / 10);
-                _putchar('0' + result % 10);
-            }
+			if (j == 0)
+			{
+				_putchar('0' + result);
+			}
+			else{
+				if (result < 10)
+					_putchar(' ');
+				else
+				_putchar('0' + result / 10);
+				_putchar('0' + result % 10);
+			}
+			if (j != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
 
-            if (j != 9)
-            {
-                _putchar(',');
-                _putchar(' ');
-            }
-        }
+			}
+		}
+		_putchar('\n');
 
-        _putchar('\n');
-    }
+	}
 }
