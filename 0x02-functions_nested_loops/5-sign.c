b055c4ff737a7819b4499ad54f8c288c50bc61a8
Ozioma EMEREM <code.emerem@gmail.com>
2023-06-20 21:09:12 +0100

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,26 +1,34 @@
 #include "main.h"
 
 /**
- * print_sign - Prints the sign of a number
- * @n: The number to check
+ * main - Entry point
  *
- * Return: 1 if n is > than zero, 0 if n is zero, -1 if n is < zero
+ * Return: Always 0 (Success)
  */
-int print_sign(int n)
+int main(void)
 {
-	if (n > 0)
-	{
-		_putchar('+');
-		return (1);
-	}
-	else if (n == 0)
-	{
-		_putchar('0');
-		return (0);
-	}
-	else
-	{
-		_putchar('-');
-		return (-1);
-	}
+	int r;
+
+	r = print_sign(98);
+	_putchar(',');
+	_putchar(' ');
+	_putchar(r + '0');
+	_putchar('\n');
+	r = print_sign(0);
+	_putchar(',');
+	_putchar(' ');
+	_putchar(r + '0');
+	_putchar('\n');
+	r = print_sign(0xff);
+	_putchar(',');
+	_putchar(' ');
+	_putchar(r + '0');
+	_putchar('\n');
+	r = print_sign(-1);
+	_putchar(',');
+	_putchar(' ');
+	_putchar(r + '0');
+	_putchar('\n');
+
+	return (0);
 }
