5fa7f8c95973538570dccfc0932c8358fdbbdc5b
Ozioma EMEREM <code.emerem@gmail.com>
2023-06-21 00:36:33 +0100

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -21,7 +21,10 @@ void times_table(void)
             else
             {
                 _putchar(' ');
-                _putchar('0' + result / 10);
+                if (result < 10)
+                    _putchar(' ');
+                else
+                    _putchar('0' + result / 10);
                 _putchar('0' + result % 10);
             }
 
