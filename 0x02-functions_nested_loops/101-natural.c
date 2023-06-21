3fbfc7876e2e7c6589e1f0a0d51277581f01942d
Ozioma EMEREM <code.emerem@gmail.com>
2023-06-21 01:54:02 +0100

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Computes and prints the sum of all the multiples
+ * of 3 or 5 below 1024 (excluded)
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int limit = 1024;
+	int sum = 0;
+	int i;
+
+	for (i = 1; i < limit; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum += i;
+		}
+	}
+
+	printf("%d, %d\n", limit, sum);
+
+	return (0);
+}
