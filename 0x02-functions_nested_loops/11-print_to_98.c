1d34364b62badf77bce761b845159ead2a25da3a
Ozioma EMEREM <code.emerem@gmail.com>
2023-06-21 01:20:25 +0100

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,14 +1,15 @@
+#include <stdlib.h>
 #include "main.h"
 
 /*
- *
+ *Prints all nubers from 0 to 98
  *
  *
  */
 void print_to_98(int n)
 {
 	for (int i = n; i <= 98; i++)
-		
+
 	{
 		printf("%d, ", i);
 	}
