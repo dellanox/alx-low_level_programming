461bb2ac19a0ac4f99a07e505e95a28da416ece9
Ozioma EMEREM <code.emerem@gmail.com>
2023-06-21 03:40:48 +0100

    104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main -fibonacciLoop - Function to print Fibonacci series
+ *
+ * @n: Number of terms to print
+ */
+void fibonacciLoop(int n)
+{
+	long long prev1 = 1;
+	long long prev2 = 2;
+	long long num;
+	int i;
+
+	if (n < 1)
+	{
+		printf("Invalid number of terms\n");
+		return;
+	}
+
+	printf("%lld, %lld", prev1, prev2);
+
+	for (i = 3; i <= n; i++)
+	{
+		num = prev1 + prev2;
+		printf(", %lld", num);
+		prev1 = prev2;
+		prev2 = num;
+	}
+
+	printf("\n");
+}
+
+int main(void)
+{
+	int n = 98;
+	fibonacciLoop(n);
+	return (0);
+}
