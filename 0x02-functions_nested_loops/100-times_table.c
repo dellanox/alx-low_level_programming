909a51985b600d3f763acf5c2a61d0d020f154ee
Ozioma EMEREM <code.emerem@gmail.com>
2023-06-21 01:40:23 +0100

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,13 +7,10 @@
  */
 void print_times_table(int n)
 {
+	int i, j, result;
+
 	if (n < 0 || n > 15)
-		return;
-
-	int i;
-	int j;
-	int result;
-
+		return();
 	for (i = 0; i <= n; i++)
 	{
 		for (j = 0; j <= n; j++)
