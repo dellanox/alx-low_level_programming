dcc6781ed33eb95ef0336d0a3245fa773948fbc9
Ozioma EMEREM <code.emerem@gmail.com>
2023-06-21 03:21:34 +0100

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+	int term1 = 1;
+	int term2 = 2;
+	int sum = 0;
+
+	while (term1 <= 4000000) {
+		if (term1 % 2 == 0) {
+			sum += term1;
+		}
+
+		int nextTerm = term1 + term2;
+		term1 = term2;
+		term2 = nextTerm;
+	}
+
+	printf("%d\n", sum);
+
+	return 0;
+
+}
