9c9a7eebdab97b22b024591f58583035f85108f5
Ozioma EMEREM <code.emerem@gmail.com>
2023-06-21 01:45:02 +0100

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,6 +11,20 @@ void print_times_table(int n)
 
 	if (n < 0 || n > 15)
 		return;
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_times_table - Prints the n times table, starting with 0
+ * @n: The number of times
+ */
+void print_times_table(int n)
+{
+	if (n < 0 || n > 15)
+		return;
+
+	int i, j, result;
+
 	for (i = 0; i <= n; i++)
 	{
 		for (j = 0; j <= n; j++)
@@ -18,13 +32,9 @@ void print_times_table(int n)
 			result = i * j;
 
 			if (j == 0)
-			{
 				printf("%d", result);
-			}
 			else
-			{
-				printf(", %d", result);
-			}
+				printf(", %3d", result);
 		}
 
 		printf("\n");
