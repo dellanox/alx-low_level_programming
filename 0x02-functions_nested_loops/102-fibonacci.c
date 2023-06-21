0f82fb392cf5b122a1aaeb2be1b3c123e3481e31
Ozioma EMEREM <code.emerem@gmail.com>
2023-06-21 17:39:42 +0100

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,30 +1,55 @@
 #include <stdio.h>
 
-void fibonacciLoop(int n)
-{
-	int fibonacci[50];
-	int i = 2;
-	fibonacci[0] = 1;
-	fibonacci[1] = 2;
-
-	if (n < 1) {
-		printf("Invalid number of terms\n");
-		return;
+/* Function to add two arrays and store the result in the third array*/
+void addArrays(int result[], int arr1[], int arr2[], int size) {
+	int carry = 0;
+	for (int i = size - 1; i >= 0; i--) {
+		int sum = arr1[i] + arr2[i] + carry;
+		result[i] = sum % 10;
+		carry = sum / 10;
 	}
-
-	printf("%d, %d", fibonacci[0], fibonacci[1]);
-
-	for (i < n; i++) {
-		fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
-		printf(", %d", fibonacci[i]);
-	}
-
-	printf("\n");
 }
 
-int main()
-{
-	int n = 50;
-	fibonacciLoop(n);
-	return 0;
+/* Function to print an array representing a number */
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d", arr[i]);
+        if (i != size - 1) {
+            printf(", ");
+        }
+    }
+    printf("\n");
+}
+
+/* Function to print the first 50 Fibonacci numbers*/
+void printFibonacciSeries() {
+	const int size = 100; /*Max number of digits for Fibonacci numbers*/
+	int fib1[size] = {0}; /*Initialize array with zeros*/
+	int fib2[size] = {0};
+	int fib3[size] = {0};
+
+	fib1[size - 1] = 1; /* Initialize fib1 with 1 */
+	fib2[size - 1] = 2; /* Initialize fib2 with 2 */
+
+	printf("The first 50 Fibonacci numbers are: ");
+	printArray(fib1, size);
+	printf(", ");
+	printArray(fib2, size);
+
+	for (int i = 3; i <= 50; i++) {
+		addArrays(fib3, fib1, fib2, size);
+		printArray(fib3, size);
+
+		// Shift the arrays to prepare for the next iteration
+		for (int j = 0; j < size; j++) {
+			fib1[j] = fib2[j];
+			fib2[j] = fib3[j];
+		}
+	}
+}
+
+/* Driver code */
+int main(void) {
+	printFibonacciSeries();
+	return (0);
 }
