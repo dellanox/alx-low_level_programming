fb9349cc83385fadaee06700273687f4cb0ac9c3
Ozioma EMEREM <code.emerem@gmail.com>
2023-06-21 01:25:51 +0100

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,17 +1,30 @@
-#include <stdlib.h>
-#include "main.h"
+#include <stdio.h>
 
-/*
- *Prints all nubers from 0 to 98
- *
- *
+/**
+ * print_to_98 - Prints all natural numbers from n to 98
+ * @n: The starting number
  */
 void print_to_98(int n)
 {
-	for (int i = n; i <= 98; i++)
+	int i;
 
+	if (n <= 98)
 	{
-		printf("%d, ", i);
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+			if (i != 98)
+				printf(", ");
+		}
+	}
+	else
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d", i);
+			if (i != 98)
+				printf(", ");
+		}
 	}
 	printf("\n");
 }
