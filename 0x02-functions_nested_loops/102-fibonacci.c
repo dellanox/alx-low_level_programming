7c9b7c90b344258761a2da6ffc9cbf89a5a3a9d5
Ozioma EMEREM <code.emerem@gmail.com>
2023-06-21 02:05:42 +0100

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,26 +9,26 @@
  */
 int main(void)
 {
-	int fibonacci[50];
-	int i;
+    int fibonacci[50];
+    int i;
 
-	fibonacci[0] = 1;
-	fibonacci[1] = 2;
+    fibonacci[0] = 1;
+    fibonacci[1] = 2;
 
-	for (i = 2; i < 50; i++)
-	{
-		fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
-	}
+    for (i = 2; i < 50; i++)
+    {
+        fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+    }
 
-	for (i = 0; i < 50; i++)
-	{
-		printf("%d", fibonacci[i]);
+    for (i = 0; i < 50; i++)
+    {
+        printf("%d", fibonacci[i]);
 
-		if (i != 49)
-			printf(", ");
-	}
+        if (i != 49)
+            printf(", ");
+    }
 
-	printf("\n");
+    printf("\n");
 
-	return 0;
+    return 0;
 }
