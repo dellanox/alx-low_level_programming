2fec96567ec93e72560c4383c11c72d5bb86a300
Ozioma EMEREM <code.emerem@gmail.com>
2023-06-21 02:58:25 +0100

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,38 +1,41 @@
-#include <stdio.h>
+include <stdio.h>
 
 /**
- * printFib - Function to print Fibonacci series
+ * fibonacciLoop - Function to print Fibonacci series
  *
  * @n: Number of terms to print
  */
 void fibonacciLoop(int n)
 {
-    int prev1 = 1;
-    int prev2 = 2;
-    int num;
-    int i;
+	int prev1 = 1;
+	int prev2 = 2;
+	int num;
+	int i;
 
-    if (n < 1) {
-        printf("Invalid number of terms\n");
-        return;
-    }
 
-    printf("The first 50 Fibonacci numbers are: ");
-    printf("%d, %d", prev1, prev2);
+	if (n < 1) {
+		printf("Invalid number of terms\n");
+		return;
+	}
 
-    for (int i = 3; i <= n; i++) {
-        num = prev1 + prev2;
-        printf(", %d", num);
-        prev1 = prev2;
-        prev2 = num;
-    }
 
-    printf("\n");
+	printf("The first 50 Fibonacci numbers are: ");
+	printf("%d, %d", prev1, prev2);
+
+	for (i = 3; i <= n; i++) {
+		num = prev1 + prev2;
+		printf(", %d", num);
+		prev1 = prev2;
+		prev2 = num;
+	}
+
+
+	printf("\n");
 }
 
 int main()
 {
-    int n = 50;
-    fibonacciLoop(n);
-    return 0;
+	int n = 50;
+	fibonacciLoop(n);
+	return 0;
 }
