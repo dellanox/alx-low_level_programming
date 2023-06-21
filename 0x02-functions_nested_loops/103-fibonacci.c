732d848c923853552fb043d3a83c526a75ad948e
Ozioma EMEREM <code.emerem@gmail.com>
2023-06-21 03:28:52 +0100

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
 
-int main() {
+/**
+ * fibonacciLoop - Function to print Fibonacci series
+ *
+ * @n: Number of terms to print
+ */
+int main(void)
+{
 	int term1 = 1;
 	int term2 = 2;
 	int sum = 0;
 	int nextTerm = 0;
 
-	while (term1 <= 4000000) {
-		if (term1 % 2 == 0) {
+	while (term1 <= 4000000)
+	{
+		if (term1 % 2 == 0)
+		{
 			sum += term1;
 		}
 
@@ -18,6 +26,6 @@ int main() {
 
 	printf("%d\n", sum);
 
-	return 0;
+	return (0);
 
 }
