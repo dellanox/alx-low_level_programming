81c5195bd09934ce61c75c1c9071dc22663f7e66
Ozioma EMEREM <code.emerem@gmail.com>
2023-06-22 16:35:02 +0100

    Move files up one level

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,36 +12,36 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-    int dayOfYear = 0;
-    int remainingDays = 0;
+	int dayOfYear = 0;
+	int remainingDays = 0;
 
-    if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
-    {
-        if (month > 2 || (month == 2 && day == 29))
-        {
-            dayOfYear = 31 + 29 + day;
-            remainingDays = 366 - dayOfYear;
-        }
-        else
-        {
-            printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
-            return;
-        }
-    }
-    else
-    {
-        if (month == 2 && day == 29)
-        {
-            printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
-            return;
-        }
-        else
-        {
-            dayOfYear = (month - 1) * 31 + day;
-            remainingDays = 365 - dayOfYear;
-        }
-    }
+	if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+	{
+		if (month > 2 || (month == 2 && day == 29))
+		{
+			dayOfYear = 31 + 29 + day;
+			remainingDays = 366 - dayOfYear;
+		}
+		else
+		{
+		printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
+		return;
+		}
+	}
+	else
+	{
+		if (month == 2 && day == 29)
+		{
+		printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
+		return;
+		}
+		else
+		{
+			dayOfYear = (month - 1) * 31 + day;
+			remainingDays = 365 - dayOfYear;
+		}
+	}
 
-    printf("Day of the year: %d\n", dayOfYear);
-    printf("Remaining days: %d\n", remainingDays);
+	printf("Day of the year: %d\n", dayOfYear);
+	printf("Remaining days: %d\n", remainingDays);
 }
