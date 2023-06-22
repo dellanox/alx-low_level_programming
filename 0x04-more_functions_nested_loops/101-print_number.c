f0a8282a5c771dee7a13561b2d3a47647f1d6ab1
Ozioma EMEREM <code.emerem@gmail.com>
2023-06-22 23:51:46 +0100

    101-print_numbers.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,11 +1,13 @@
 #include "main.h"
 
 /**
- * print_number - Prints an integer.
- * @n: The integer to be printed.
+ * print_number - Prints an integer
+ * @n: The integer to be printed
  */
 void print_number(int n)
 {
+	unsigned int num;
+
 	if (n == 0)
 	{
 		_putchar('0');
@@ -15,11 +17,17 @@ void print_number(int n)
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		num = -n;
+	}
+	else
+	{
+		num = n;
 	}
 
-	if (n / 10)
-		print_number(n / 10);
+	if (num / 10 != 0)
+	{
+		print_number(num / 10);
+	}
 
-	_putchar('0' + (n % 10));
+	_putchar((num % 10) + '0');
 }
