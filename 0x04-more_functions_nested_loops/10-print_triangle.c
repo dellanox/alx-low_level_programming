a465c4fd39a5093068b4360e93a2b8e4e5d8387f
Ozioma EMEREM <code.emerem@gmail.com>
2023-06-22 19:59:13 +0100

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
 /**
- * print_triangle - Prints a triangle using the # character
- *
+ * print_triangle - Prints a triangle using the # character with indentation
  * @size: The size of the triangle
  */
 void print_triangle(int size)
 {
-	int i, j;
+	int i, j, spaces;
 
 	if (size <= 0)
 	{
@@ -17,7 +16,12 @@ void print_triangle(int size)
 
 	for (i = 1; i <= size; i++)
 	{
-		for (j = 1; j <= i; j++)
+		spaces = size - i;
+
+		for (j = 0; j < spaces; j++)
+			_putchar(' ');
+
+		for (j = 0; j < i; j++)
 			_putchar('#');
 
 		_putchar('\n');
