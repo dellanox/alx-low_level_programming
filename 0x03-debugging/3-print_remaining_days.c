b0d5768c60739efc4080ddcea59b43cd84403207
Ozioma EMEREM <code.emerem@gmail.com>
2023-06-22 01:21:27 +0100

    3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -9,25 +9,36 @@
 * @year: year
 * Return: void
 */
-
 void print_remaining_days(int month, int day, int year)
 {
+	int days_in_month[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	int total_days = 0;
+	int i;
+
 	if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
 	{
-		if (month > 2 || (month == 2 && day == 29))
-		{
-			printf("Day of the year: %d\n", day + 1);
-			printf("Remaining days: %d\n", 366 - (day + 1));
-		}
-		else
-		{
-			printf("Day of the year: %d\n", day);
-			printf("Remaining days: %d\n", 366 - day);
-		}
+		days_in_month[2] = 29;
 	}
-	else
+
+	for (i = 1; i < month; i++)
 	{
-		printf("Day of the year: %d\n", day);
-		printf("Remaining days: %d\n", 365 - day);
+		total_days += days_in_month[i];
 	}
+	total_days += day;
+
+	if (month < 1 || month > 12 || day < 1 || day > days_in_month[month])
+	{
+		printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
+		return;
+	}
+
+
+	int remaining_days = 365 - total_days;
+	if (days_in_month[2] == 29 && month <= 2 && day <= 29)
+	{
+		remaining_days++;
+	}
+
+	printf("Day of the year: %d\n", total_days);
+	printf("Remaining days: %d\n", remaining_days);
 }
