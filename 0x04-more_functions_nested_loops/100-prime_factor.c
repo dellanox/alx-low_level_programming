972fc80c2fd2a1389a4db76b339e8f16bf21926f
Ozioma EMEREM <code.emerem@gmail.com>
2023-06-22 20:06:29 +0100

    100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * largest_prime_factor - Finds the largest prime factor of a given number
+ * @n: The number to find the largest prime factor of
+ *
+ * Return: The largest prime factor of the number
+ */
+long largest_prime_factor(long n)
+{
+	long i;
+
+	while (n % 2 == 0)
+		n /= 2;
+
+	for (i = 3; i <= sqrt(n); i += 2)
+	{
+		while (n % i == 0)
+		{
+			n /= i;
+		}
+	}
+
+	return (n > 2) ? n : 2;
+}
+
+int main(void)
+{
+	long number = 612852475143;
+	long largest_factor = largest_prime_factor(number);
+
+	printf("%ld\n", largest_factor);
+
+	return 0;
+}
