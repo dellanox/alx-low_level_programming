a6494596fdf3446c2a015c5aa554a9dc26639ad9
Ozioma EMEREM <code.emerem@gmail.com>
2023-06-23 01:35:50 +0100

    3-print_remaining_days.c

--- 0x03-debugging/0x04-more_functions_nested_loops/README.md
+++
@@ -1,1 +0,0 @@
-Readme for 0x04-more_functions_nested_loops
--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -2,30 +2,25 @@
 #include "main.h"
 
 /**
-* print_remaining_days - takes a date and prints how many days are
-* left in the year, taking leap years into account
-* @month: month in number format
-* @day: day of month
-* @year: year
-* Return: void
-*/
-
+ * print_remaining_days - takes a date and prints how many days are
+ * left in the year, taking leap years into account
+ * @month: month in number format
+ * @day: day of month
+ * @year: year
+ * Return: void
+ */
 void print_remaining_days(int month, int day, int year)
 {
-	int dayOfYear = 0;
-	int remainingDays = 0;
-
 	if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
 	{
 		if (month > 2 || (month == 2 && day == 29))
 		{
-			dayOfYear = 31 + 29 + day;
-			remainingDays = 366 - dayOfYear;
+		printf("Day of the year: %d\n", day + 1);
+		printf("Remaining days: %d\n", 366 - (day + 1));
 		}
 		else
 		{
 		printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
-		return;
 		}
 	}
 	else
@@ -33,15 +28,11 @@ void print_remaining_days(int month, int day, int year)
 		if (month == 2 && day == 29)
 		{
 		printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
-		return;
 		}
 		else
 		{
-			dayOfYear = (month - 1) * 31 + day;
-			remainingDays = 365 - dayOfYear;
+			printf("Day of the year: %d\n", day);
+			printf("Remaining days: %d\n", 365 - day);
 		}
 	}
-
-	printf("Day of the year: %d\n", dayOfYear);
-	printf("Remaining days: %d\n", remainingDays);
 }
