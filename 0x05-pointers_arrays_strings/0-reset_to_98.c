043d0222f41b49dd745d0d3d16f5a21945477ab9
Ozioma EMEREM <code.emerem@gmail.com>
2023-06-26 19:58:36 +0100

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/**
+ * reset_to_98 - Updates the value pointed to by a pointer to 98
+ * @n: Pointer to an int to update
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Readme Documentation for 0x05-pointers_arrays_strings
