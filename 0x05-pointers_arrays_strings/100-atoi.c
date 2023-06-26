fe94d7be57441490d131baacc51c3500da9d998f
Ozioma EMEREM <code.emerem@gmail.com>
2023-06-27 00:52:35 +0100

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,7 +1,7 @@
 #include "main.h"
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <limits.h>
 
 /**
  * _atoi - Converts a string to an integer.
@@ -15,7 +15,6 @@ int _atoi(char *s)
 	int sign = 1;
 	int started = 0;
 
-
 	while (*s != '\0')
 	{
 		if (*s == '-')
@@ -24,24 +23,24 @@ int _atoi(char *s)
 		if (*s >= '0' && *s <= '9')
 		{
 			if (result > INT_MAX / 10 ||
-			    (result == INT_MAX / 10 && (*s - '0') > INT_MAX % 10))
+			    (result == INT_MAX / 10 &&
+			     (*s - '0') > INT_MAX % 10))
 			{
 				if (sign == 1)
 					return (INT_MAX);
 				else
 					return (INT_MIN);
 			}
+
 			result = result * 10 + (*s - '0');
 			started = 1;
 		}
 		else if (started)
 		{
 			break;
-
-
-			s++;
 		}
 
-		return (sign * result);
+		s++;
 	}
+	return (sign * result);
 }
