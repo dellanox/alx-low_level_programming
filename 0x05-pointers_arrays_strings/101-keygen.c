57e2f97e231f618a3f66b437bf23b218a4f7af1c
Ozioma EMEREM <code.emerem@gmail.com>
2023-06-27 01:06:17 +0100

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -37,3 +37,34 @@ int main(void)
 
 	return (0);
 }
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+#define PASSWORD_LENGTH 15
+
+/**
+ * main - Generates a random valid password for 101-crackme.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char password[PASSWORD_LENGTH + 1];
+	int i;
+	time_t t;
+
+	srand((unsigned int)time(&t));
+
+	for (i = 0; i < PASSWORD_LENGTH; i++)
+	{
+		password[i] = rand() % ('~' - '!') + '!';
+	}
+
+	password[PASSWORD_LENGTH] = '\0';
+
+	printf("%s\n", password);
+
+	return (0);
+}
