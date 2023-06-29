6096ccf9da2940191df75e27effbe0fec69067c8
Ozioma EMEREM <code.emerem@gmail.com>
2023-06-30 00:14:23 +0100

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,19 +1,12 @@
 #include "main.h"
 
 /**
- * print_number - Prints an integer.
- * @n: The integer to print.
+ * print_number - Prints an integer using _putchar function
+ * @n: The integer to be printed
  */
 void print_number(int n)
 {
-	int divisor = 1;
-	int temp = n;
-
-	if (n == 0)
-	{
-		_putchar('0');
-		return;
-	}
+	int digit;
 
 	if (n < 0)
 	{
@@ -21,39 +14,16 @@ void print_number(int n)
 		n = -n;
 	}
 
-
-	while (temp / 10 != 0)
+	if (n == 0)
 	{
-		divisor *= 10;
-		temp /= 10;
+		_putchar('0');
+		return;
 	}
 
-	while (divisor != 0)
+	while (n > 0)
 	{
-		int digit = n / divisor;
-
-		_putchar(digit + '0');
-		n %= divisor;
-		divisor /= 10;
+		digit = n % 10;
+		_putchar('0' + digit);
+		n /= 10;
 	}
-}
-void print_number(int n)
-{
-  if (n < 0) {
-    _putchar('-');
-    n = -n;
-  }
-
-  if (n == 0) {
-    _putchar('0');
-    eturn;
-
-  }
-
-  int digit;
-  while (n > 0) {
-    digit = n % 10;
-    _putchar('0' + digit);
-    n /= 10;
-  }
 }
