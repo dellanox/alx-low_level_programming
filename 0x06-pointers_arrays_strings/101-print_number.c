9a8dd83ab12da09583db298715aaba7816e740a2
Ozioma EMEREM <code.emerem@gmail.com>
2023-06-30 00:44:34 +0100

    101-print_number

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,21 +6,14 @@
  */
 void print_number(int n)
 {
-	int divisor = 1;
-
 	if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
 	}
 
-	while (n / divisor >= 10)
-		divisor *= 10;
+	if (n / 10 != 0)
+		print_number(n / 10);
 
-	while (divisor > 0)
-	{
-		_putchar('0' + (n / divisor));
-		n %= divisor;
-		divisor /= 10;
-	}
+	_putchar('0' + (n % 10));
 }
