ed0b5e84e810992d2353a3f59e8aebf3ba249144
Ozioma EMEREM <code.emerem@gmail.com>
2023-06-30 00:40:03 +0100

    101-print_number

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -4,22 +4,23 @@
  * print_number - Prints an integer using _putchar function
  * @n: The integer to be printed
  */
+void print_number(int n)
+{
+	int divisor = 1;
 
-void print_number(int n) {
-  if (n < 0) {
-    _putchar('-');
-    n = -n;
-  }
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
 
-  if (n == 0) {
-    _putchar('0');
-    return;
-  }
+	while (n / divisor >= 10)
+		divisor *= 10;
 
-  int digit;
-  do {
-    digit = n % 10;
-    _putchar('0' + digit);
-    n /= 10;
-  } while (n > 0);
+	while (divisor > 0)
+	{
+		_putchar('0' + (n / divisor));
+		n %= divisor;
+		divisor /= 10;
+	}
 }
