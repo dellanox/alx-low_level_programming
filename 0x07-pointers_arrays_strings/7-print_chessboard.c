5d14d6569762946290df92a600d80eaf81462e7a
Ozioma EMEREM <code.emerem@gmail.com>
2023-07-03 20:05:13 +0100

    7-print_chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -12,6 +12,10 @@ void print_chessboard(char (*a)[8])
 	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
-			printf("%c%c", a[i][j], (j != 7) ? ' ' : '\n');
+		{
+			putchar(a[i][j]);
+			putchar(' ');
+		}
+		putchar('\n');
 	}
 }
