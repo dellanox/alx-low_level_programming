8d480ca9de62774ed412b98bbffabf485d60efc5
Ozioma EMEREM <code.emerem@gmail.com>
2023-07-03 19:08:11 +0100

    3-strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,3 +1,6 @@
+#include "main.h"
+#include <string.h>
+
 /**
  * _strspn - Gets the length of a prefix substring
  * @s: Pointer to the string
@@ -9,27 +12,11 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int count = 0;
-	int match;
 
-	while (*s)
+	while (*s && strchr(accept, *s))
 	{
-		match = 0;
-		while (*accept)
-		{
-			if (*s == *accept)
-			{
-				match = 1;
-				break;
-			}
-			accept++;
-		}
-
-		if (!match)
-			break;
-
 		count++;
 		s++;
-		accept = accept - count;
 	}
 
 	return (count);
