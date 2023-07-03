2cd5acb57aeb2a64fd412c7ffafe8ffdf8b73739
Ozioma EMEREM <code.emerem@gmail.com>
2023-07-03 16:58:57 +0100

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,17 +8,16 @@
  *
  * Return: Pointer to the memory area s
  */
-
-
 char *_memset(char *s, char b, unsigned int n)
 {
-    char *ptr = s;
+	char *ptr = s;
 
-    while (n > 0) {
-        *ptr = b;
-        ptr++;
-        n--;
-    }
+	while (n > 0)
+	{
+		*ptr = b;
+		ptr++;
+		n--;
+	}
 
-    return (s);
+	return (s);
 }
