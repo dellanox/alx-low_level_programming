71cda0fb0577e860e8a4bcc66fbe59e4f0ef3ab9
Ozioma EMEREM <code.emerem@gmail.com>
2023-07-04 11:04:38 +0100

    7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -3,7 +3,7 @@
 
 /**
  * print_chessboard - Prints the chessboard
- * @a: 2D array representing the chessboard
+ * @a: Pointer to the chessboard array
  */
 void print_chessboard(char (*a)[8])
 {
@@ -14,7 +14,6 @@ void print_chessboard(char (*a)[8])
 		for (j = 0; j < 8; j++)
 		{
 			_putchar(a[i][j]);
-			_putchar(' ');
 		}
 		_putchar('\n');
 	}
