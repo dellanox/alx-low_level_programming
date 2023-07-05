55085b8a765ad8641559adef58b6cd13bc6c99e3
Ozioma EMEREM <code.emerem@gmail.com>
2023-07-05 21:48:15 +0100

    101-wildcmp

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -13,5 +13,24 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 void print_number(int num);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
+
+
+
+
+
+
+
+
 
 #endif /* MAIN_H */
