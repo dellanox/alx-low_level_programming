bc37d5b3be979865e0c1dce3b162fcace0c8c47d
Ozioma EMEREM <code.emerem@gmail.com>
2023-07-14 01:40:39 +0100

    3-array_range

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -44,7 +44,7 @@ char *rot13(char *);
 void print_number(int n);
 
 int _putchar(char c);
-void simple_print_buffer(char *buffer, unsigned int size);
+/*void simple_print_buffer(char *buffer, unsigned int size);*/
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
@@ -57,7 +57,7 @@ void set_string(char **s, char *to);
 void print_number(int num);
 
 int _putchar(char c);
-void simple_print_buffer(char *buffer, unsigned int size);
+/*void simple_print_buffer(char *buffer, unsigned int size);*/
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
