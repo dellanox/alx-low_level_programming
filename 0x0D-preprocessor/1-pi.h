b399495f410fa24bc6a278ae3154724e99e9657d
Ozioma EMEREM <code.emerem@gmail.com>
2023-07-17 22:49:46 +0100

    1-pi.h

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,12 @@
+#ifndef SUM_H
+#define SUM_H
+
+/**
+ * File: 1-pi.h
+ * Description: Header file that defines function-like macro 1-pi.h
+ * to compute the sum of two numbers.
+ */
+
+#define PI 3.14159265359
+
+#endif /* SUM_H */
