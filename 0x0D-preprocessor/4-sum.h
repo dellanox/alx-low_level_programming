0be89a369ccc95a94582187cfe2576b7c2ed5028
Ozioma EMEREM <code.emerem@gmail.com>
2023-07-17 23:03:35 +0100

    4-sum.h

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,11 +1,6 @@
 #ifndef SUM_H
 #define SUM_H
 
-/**
- * File: 4-sum.h
- * Description: Header file that defines function-like macro SUM(x, y)
- * to compute the sum of two numbers.
- */
-
 #define SUM(x, y) ((x) + (y))
-#endif /* SUM_H */
+
+#endif 
