081174ba735a96c13bd0bd2cd665315b1612153c
Ozioma EMEREM <code.emerem@gmail.com>
2023-07-27 22:36:01 +0100

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/**
+ * startup_print - Function to be executed before main.
+ * This function prints "You're beat! and yet, you must allow,\nI bore my house upon my back!\n".
+ */
+void __attribute__((constructor)) startup_print(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
