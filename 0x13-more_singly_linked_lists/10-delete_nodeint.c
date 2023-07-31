828b53a6cec6c3c4eca3c86b242de40c76330444
Ozioma EMEREM <code.emerem@gmail.com>
2023-07-31 18:34:29 +0100

    10-delete_nodeint.c

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - Deletes the node at a given index in the list.
+ * @head: Pointer to the pointer of the head of the list.
+ * @index: Index of the node to be deleted (starting from 0).
+ *
+ * Return: 1 if deletion succeeded, -1 if it failed.
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *current, *temp;
+	unsigned int count = 0;
+
+	if (head == NULL || *head == NULL)
+		return (-1);
+
+	if (index == 0)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+		return (1);
+	}
+
+	current = *head;
+
+	while (current != NULL && count < index - 1)
+	{
+		current = current->next;
+		count++;
+	}
+
+	if (current == NULL || current->next == NULL)
+		return (-1);
+
+	temp = current->next;
+	current->next = temp->next;
+	free(temp);
+
+	return (1);
+}
---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,64 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * add_nodeint - Adds a new node at the beginning of a listint_t list.
+ * @head: Pointer to the pointer of the head of the list.
+ * @n: The value to be stored in the new node.
+ *
+ * Return: The address of the new node (listint_t), or NULL if it failed.
+ */
+listint_t *add_nodeint(listint_t **head, int n)
+{
+	listint_t *new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
+}
+
+/**
+ * insert_nodeint_at_index - Inserts a new node at a given position in the list.
+ * @head: Pointer to the pointer of the head of the list.
+ * @idx: Index where the new node should be added (starting from 0).
+ * @n: The value to be stored in the new node.
+ *
+ * Return: The address of the new node (listint_t), or NULL if it failed.
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *new_node, *prev_node;
+	unsigned int count = 0;
+
+	if (head == NULL)
+		return (NULL);
+
+	if (idx == 0)
+		return add_nodeint(head, n);
+
+	prev_node = *head;
+
+	while (prev_node != NULL && count < idx - 1)
+	{
+		prev_node = prev_node->next;
+		count++;
+	}
+
+	if (prev_node == NULL || prev_node->next == NULL)
+		return (NULL);
+
+	new_node = malloc(sizeof(listint_t));
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = prev_node->next;
+	prev_node->next = new_node;
+
+	return (new_node);
+}
