ec6eaf43c33c5306fce652a4aefd72cd4c55cb00
Ozioma EMEREM <code.emerem@gmail.com>
2023-08-03 17:42:13 +0100

    Added test files (*.main files)

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Function prototypes and any necessary includes here */
+
+/* Example function prototypes */
+int add(int a, int b);
+void print_binary(unsigned int num);
+
+#endif /* MAIN_H */
