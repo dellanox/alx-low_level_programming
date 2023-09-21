19a7bc3af167dcc120bb4085239c8e094c7f21f0
Ozioma EMEREM <code.emerem@gmail.com>
2023-09-21 01:00:02 +0100

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,35 +1,36 @@
-#include"main.h"
+#include "main.h"
 
 /**
- *append_text_to_file - appends text at the end of a file
- *@filename: A pointer to the name of the file.
- *@text_content: The string to add to the end of the file.
+ * append_text_to_file - Appends text to the end of a file.
+ * @filename: The name of the file.
+ * @text_content: The NULL-terminated string to append to the file.
  *
- *Return: If the function fails or filename is NULL -1
- *	if the file does not exist the user lacks write permissions -1
- *	otherwise 1
+ * Return: 1 on success, -1 on failure.
  */
-
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int o, w, len = 0;
+	int file_descriptor, write_count, length = 0;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
 
-	if (text_content != NULL)
+	file_descriptor = open(filename, O_WRONLY | O_APPEND);
+	if (file_descriptor == -1)
+		return (-1);
+
+	if (text_content)
 	{
-		for (len = 0; text_content[len];)
-			len++;
+		while (text_content[length])
+			length++;
+
+		write_count = write(file_descriptor, text_content, length);
+		if (write_count == -1 || write_count != length)
+		{
+			close(file_descriptor);
+			return (-1);
+		}
 	}
 
-	o = open(filename, O_WRONLY | O_APPEND);
-	w = write(o, text_content, len);
-
-	if (o == -1 || w == -1)
-		return (-1);
-
-	close(o);
-
+	close(file_descriptor);
 	return (1);
 }
