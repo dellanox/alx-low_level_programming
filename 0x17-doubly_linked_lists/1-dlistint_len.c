bb152d582b4e2a46e59b371000f80dd9e0c90cb2
Ozioma EMEREM <code.emerem@gmail.com>
2023-10-12 23:01:47 +0100

    1-dlistint_len.c

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,29 +1,20 @@
 #include "lists.h"
 
 /**
- * dlistint_len - returns the number of elements in
- * a double linked list
- *
- * @h: head of the list
- * Return: the number of nodes
+ * dlistint_len - get number of nodes
+ * @h: the head of list
+ * Return: number of nodes
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-	int count;
+	/* declarations */
+	int nodes = 0;
 
-	count = 0;
-
-	if (h == NULL)
-		return (count);
-
-	while (h->prev != NULL)
-		h = h->prev;
-
-	while (h != NULL)
+	/* count nodes as we move through list */
+	while (h)
 	{
-		count++;
+		nodes += 1;
 		h = h->next;
 	}
-
-	return (count);
+	return (nodes);
 }
